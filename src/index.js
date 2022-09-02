54e5d4fc0da850f8266bf72d5c296538c9e3e8c9
jodaraca <josedavidrc59@gmail.com>
2022-09-02 08:39:14 -0400

    se incluyen cambios para netlify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Template from '@templates/Template.js';
 import '@styles/main.css';
-
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
